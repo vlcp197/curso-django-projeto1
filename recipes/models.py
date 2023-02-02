bf08abdcf119365be2f2e3a217fce47e7f7d319b
vlcp197 <vlcp197@gmail.com>
2023-02-01 21:59:47 -0300

    Primeira migration criada

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,11 @@
+from __future__ import annotations
+
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
 
 
 class Recipe(models.Model):
@@ -6,12 +13,18 @@ class Recipe(models.Model):
     description = models.CharField(max_length=165)
     slug = models.SlugField()
     preparation_time = models.IntegerField()
-    preparation_time_unit = models.Charfield(max_length=65)
+    preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
-    servings_unit = models.Charfield(max_length=65)
+    servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        'Category', on_delete=models.SET_NULL, null=True,
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True,
+    )
