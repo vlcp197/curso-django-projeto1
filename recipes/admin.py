3d1c05e629dd4083360ff4b343606b450930eccd
vlcp197 <vlcp197@gmail.com>
2023-02-02 22:14:36 -0300

    Registrando Category na Admin do Django

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Category, Recipe
+
+
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(model_or_iterable=Category,
+                    admin_class=CategoryAdmin)
+admin.site.register(model_or_iterable=Recipe,
+                    admin_class=RecipeAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -7,10 +7,13 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
-    description = models.CharField(max_length=165)
+    descripution = models.CharField(max_length=165)
     slug = models.SlugField()
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
@@ -28,3 +31,6 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True,
     )
+
+    def __str__(self):
+        return self.title
